0ef0f651c6c777d4b78b75c9e616b0e8c622d87a
cytcool <675149948@qq.com>
2018-05-17 21:45:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cyt/appbusiness/CommonOkHttpClient.java
@@ -0,0 +1,58 @@
+package com.example.cyt.appbusiness;
+
+import com.example.cyt.appbusiness.okhttp.https.HttpsUtils;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by CYT on 2018/5/14.
+ * @function 请求的发送，请求参数的配置，https支持
+ */
+
+public class CommonOkHttpClient {
+
+    private static final int TIME_OUT = 30;//超时参数
+    private static OkHttpClient mOkHttpClient;
+
+    //为client配置参数
+    static {
+
+        //创建client对象的构建者
+        OkHttpClient.Builder okHttpBuilder = new OkHttpClient.Builder();
+        //为构建者填充超时时间
+        okHttpBuilder.connectTimeout(TIME_OUT, TimeUnit.SECONDS);
+        okHttpBuilder.readTimeout(TIME_OUT,TimeUnit.SECONDS);
+        okHttpBuilder.writeTimeout(TIME_OUT,TimeUnit.SECONDS);
+
+        okHttpBuilder.followRedirects(true);
+
+        //https支持
+        okHttpBuilder.hostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String s, SSLSession sslSession) {
+                return true;
+            }
+        });
+        okHttpBuilder.sslSocketFactory(HttpsUtils.getSslSocketFactory());
+
+        //生成client对象
+        mOkHttpClient = okHttpBuilder.build();
+    }
+
+    public static Call sendRequest(Request request, Callback commCallback){
+
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(commCallback);
+
+        return call;
+    }
+
+}
